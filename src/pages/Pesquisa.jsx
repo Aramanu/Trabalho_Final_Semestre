09db7ec9750270d64346267e9873c255c44d55f9
ARAMANU ROCHA DE OLIVEIRA <136847086+Aramanu@users.noreply.github.com>
2025-11-24 10:40:21 -0300

    acerto pagina pesquisa

--- src/pages/Pesquisa.jsx
+++ src/pages/Pesquisa.jsx
@@ -56,33 +56,35 @@ export default function Pesquisa() {
   
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Cabecalho />
-      <div className="mt-30 md:mt-35 flex flex-col items-center gap-8  ">
-        <h1 className="text-4xl font-bold flex justify-center my-10e">
-          Pesquisa Produtos
-        </h1>
-        <form className="flex flex-col gap-4 items-center" onSubmit={handleSubmit(pesquisaArtigos)}>
-          <input
-            type="text"
-            className="w-80 md:w-96"
-            required
-            placeholder="Palavra chave do artigo"
-            {...register("palavraChave")}
-          />
-          <input
-            type="submit"
-            value="Pesquisar"
-            className="bg-verde hover:bg-laranja text-preto_azulado font-bold py-3 px-6 rounded duration-200 w-72 md:w-80"
-          />
-        </form>
-      </div>
-      <section className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center md:place-items-start">
-          {listaResultados}
+      <main className="flex-1">
+        <div className="mt-30 md:mt-35 flex flex-col items-center gap-8  ">
+          <h1 className="text-4xl font-bold flex justify-center my-10e">
+            Pesquisa Produtos
+          </h1>
+          <form className="flex flex-col gap-4 items-center" onSubmit={handleSubmit(pesquisaArtigos)}>
+            <input
+              type="text"
+              className="w-80 md:w-96"
+              required
+              placeholder="Palavra chave do artigo"
+              {...register("palavraChave")}
+            />
+            <input
+              type="submit"
+              value="Pesquisar"
+              className="bg-verde hover:bg-laranja text-preto_azulado font-bold py-3 px-6 rounded duration-200 w-72 md:w-80"
+            />
+          </form>
         </div>
-      </section>
+        <section className="container mx-auto px-4 py-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center md:place-items-start">
+            {listaResultados}
+          </div>
+        </section>
+      </main>
       <RodaPe />
-    </>
+    </div>
   );
 }
